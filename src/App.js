e76b0950b523ededeb7ce3d59133decd967bcf7b
RafayelKarapetyan <karapetyanrafayel2@gmail.com>
2023-04-12 11:18:59 +0400

    ReactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Input({type, placeholder}){  
+  return (<input type={type} placeholder={placeholder}/>);
+}
+
+function Button({value, onClick}){  
+  return (<button onclick={onClick}> {value}  </button>);
+}
+
+function generator(){
+  const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789()+-.";
+  let password = "";
+  for (let i = 0; i < 12; i++) {
+      password += symbols.charAt(Math.floor(Math.random() * symbols.length));
+  }
+  alert(password);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Signup">
+      <h4>Sign Up to get more information</h4>
+      <form>
+        <Input type="text" placeholder="Name"/>
+        <Input type="text" placeholder="Last Name"/>
+        <Input type="number" placeholder="age"/>
+        <Input type="email" placeholder="Email"/>
+        <Input type="password" placeholder="Password"/>
+        <Button value="Use password generator" onClick={generator()}/>
+      </form>
     </div>
   );
 }
+
+
+
 
 export default App;
